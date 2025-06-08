aa636446019d67202724418aabf7d219e03d6bc0
aibobotclub <aida_bot@outlook.com>
2025-06-09 02:25:18 +0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,54 +7,13 @@ export function Footer() {
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="text-center md:text-left mb-4 md:mb-0">
             <p className="text-gray-500">
-              Powered by{" "}
-              <a
-                href="https://togetherai.link/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-500 hover:text-gray-900 transition-colors  underline-offset-4 underline"
-              >
-                Together AI
-              </a>
-              . Created by{" "}
-              <a
-                href="https://twitter.com/nutlope"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-500 hover:text-gray-900 transition-colors  underline-offset-4 underline"
-              >
-                Hassan.
-              </a>
+              Powered by AIDA AI.
             </p>
           </div>
           <div className="flex space-x-4">
-            <a
-              href="https://github.com/Nutlope/picmenu"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-600 hover:text-gray-800 transition-colors"
-            >
-              <FaGithub className="h-6 w-6" />
-              <span className="sr-only">GitHub</span>
-            </a>
-            <a
-              href="https://twitter.com/nutlope"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-600 hover:text-gray-800 transition-colors"
-            >
-              <FaTwitter className="h-6 w-6" />
-              <span className="sr-only">Twitter</span>
-            </a>
-            <a
-              href="https://linkedin.com/in/nutlope"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-600 hover:text-gray-800 transition-colors"
-            >
-              <FaLinkedin className="h-6 w-6" />
-              <span className="sr-only">LinkedIn</span>
-            </a>
+            <FaGithub className="h-6 w-6 text-gray-400" />
+            <FaTwitter className="h-6 w-6 text-gray-400" />
+            <FaLinkedin className="h-6 w-6 text-gray-400" />
           </div>
         </div>
       </div>
